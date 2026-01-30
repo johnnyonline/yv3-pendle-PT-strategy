b2d0cb4b27d435a1cb68ad927921dc140b670866
johnnyonline <johnnyonline.eth@gmail.com>
2026-01-30 16:40:07 -0300

    chore: remove unused ERC20

--- src/StrategyFactory.sol
+++ src/StrategyFactory.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.18;
 
-import {PendlePTStrategy as Strategy, ERC20} from "./Strategy.sol";
+import {PendlePTStrategy as Strategy} from "./Strategy.sol";
 import {IStrategyInterface} from "./interfaces/IStrategyInterface.sol";
 
 contract StrategyFactory {
